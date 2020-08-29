c8a44c6225f9c55eca8a5f3cab68a2b091baf708
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-30 06:52:31 +0800

    message sender sectioin structure

--- src/components/Feed/MessageSender/MessageSender.js
+++ src/components/Feed/MessageSender/MessageSender.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './MessageSender.css'
 
+// icons
+import { Avatar } from '@material-ui/core'
+import { Videocam, PhotoLibrary, InsertEmoticon} from '@material-ui/icons'
+
 const MessageSender = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
     return (
         <div className="messageSender">
-            <div className="messageSenderTop"></div>
-            <div className="messageSenderTop"></div>
+            <div className="messageSenderTop">
+                <Avatar />
+                <form>
+                    <input className="messageSenderInput" placeholder={`What's on your mind?`} />
+                    <input placeholder={"Image URL (Optional)"} />
+                    <button onClick={handleSubmit} type="submit">Hidden submit</button>
+                </form>
+            </div>
+            <div className="messageSenderTop">
+                <div className="messageSenderOption">
+                    <Videocam style={{color: 'red'}} />
+                    <h3>Live Video</h3>
+                </div>
+
+                <div className="messageSenderOption">
+                    <PhotoLibrary style={{color: 'green'}} />
+                    <h3>Photo/Video</h3>
+                </div>
+
+                <div className="messageSenderOption">
+                    <InsertEmoticon style={{color: 'orange'}} />
+                    <h3>Feeling/Activity</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Feed/StoryReel/Story/Story.js
+++ src/components/Feed/StoryReel/Story/Story.js
@@ -4,7 +4,7 @@ import { Avatar } from '@material-ui/core';
 
 const Story = ({ image, profileSrc, title }) => {
     return (
-        <div style={{backgroundImage: `url(${image})`}} className="story">
+        <div className="story" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${image})`}}>
             <Avatar src={profileSrc} className="storyAvatar" />
             <h4>{title}</h4>
         </div>
